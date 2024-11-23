ceb4b413ceb377640b8253e0b9f1df7d6f84f8d7
ORIANEESTHER <orianenganga14@gmail.com>
2024-11-23 22:29:04 +0100

    debutFront

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import LoginPage from './components/templates/LoginPage';
+import LoginPage from './components/pages/LoginPage';
+import CoursesPage from './components/pages/CoursesPage';
 
-const App = () => {
+const App: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LoginPage />} />
+        <Route path="/courses" element={<CoursesPage />} />
       </Routes>
     </Router>
   );
